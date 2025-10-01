9da7cb9a68781382a13753568ece0494dce0d9bd
Harish9907 <thandilharish@gmail.com>
2025-10-01 02:32:06 +0000

    Fix predict.py to load model.joblib and run prediction

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,8 @@
 import joblib
 import numpy as np
 
-model = joblib.load("models/model.pkl")
-X = np.array([[5.1, 3.5, 1.4, 0.2]])
+model = joblib.load("models/model.joblib")
+X = np.array([[5.1, 3.5]])
 pred = model.predict(X)
 print("Prediction:", pred[0])
 
